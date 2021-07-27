179e2cdcae2283dde8db245f7ea92e2b6c78b953
Arun Baby <arunbabyaj@cet.ac.in>
2021-07-27 18:36:26 +0530

    simple interest calculation

---
+++ Basics/si.c
@@ -0,0 +1,12 @@
+
+
+#include<stdio.h>
+void main()
+{
+    int p,n,r=8,si;
+    
+    printf("Enter Principle,Time :");
+    scanf("%d%d",&p,&n);
+    si=(p*r*n)/100;
+    printf("Simple Interest is :%d",si);
+}
